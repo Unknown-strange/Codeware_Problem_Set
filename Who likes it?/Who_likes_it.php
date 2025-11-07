d3acfd590e4769a97e45122ca98095d6b0ba74e7
Edwin <princenyarkoedwin@gmail.com>
2025-11-07 08:52:43 +0000

    Implement likes function to handle user likes

---
+++ Who likes it?/Who_likes_it.php
@@ -0,0 +1,27 @@
+function likes( $names ) {
+  $array = $names;
+  if (count($array) == 1){
+    $result = implode('',$array);
+    return $result . " likes this";
+  }
+  else if (count($array) == 2){
+    $result = implode(" and ", $array);
+    return $result . " like this";
+  }
+  else if( count($array) === 3){
+    $last = array_pop($array);
+    $firstpart = implode(', ',$array);
+    $final = $firstpart . " and " . $last . " like this";
+    return $final;
+  }
+  else if(count($array) > 3){
+    $first_two = array_slice($array,0,2);
+    $result = implode(', ', $first_two);
+    $final = $result . " and 2 others like this";
+    return $final;
+  }
+  else{
+    return 'no one likes this';
+  }
+  
+}
