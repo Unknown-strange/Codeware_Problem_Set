16bebc462aa1e99133260cbd415592a04e8767d4
Edwin <princenyarkoedwin@gmail.com>
2025-11-17 11:37:23 +0000

    Create code.php

---
+++ Valid Braces/code.php
@@ -0,0 +1,25 @@
+function validBraces($braces){
+  $chars = str_split($braces);
+  $stack = [];
+  $pair = [
+    '(' => ')',
+    '{' => '}',
+    '[' => ']',
+  ];
+    
+  for ($i = 0; $i < count($chars); $i++){
+    $char = $chars[$i];
+    
+    if(in_array($char, array_keys($pair))){
+      array_push($stack, $char);
+    }
+    else{
+      $last = array_pop($stack);
+      if ($char !== $pair[$last]){
+        return false;
+      }
+    }
+  }
+  
+  return empty($stack);
+}
